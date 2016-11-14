3e6fa09b74b3b5c852708214043ccb16d0ff6921
Hamid Shahrokhi <Hamid Shahrokhi>
2016-11-13 22:37:28 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,13 +15,37 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: `Weclone to the chat app`,
+		createAt: new Date().getTime()
+	})
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: `New user joined`,
+		createAt: new Date().getTime()
+	});	
+
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
-		io.emit('newMessage', {
+
+		io.emit('createMessage', (message) => {
+			console.log('createMessage', message);
+			io.emit('newMessage', {
+				from: message.from,
+				text: message.text,
+				createAt: new Date().getTime()
+			});
+		});
+
+/*
+		socket.broadcast.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createAt: new Date().getTime()
 		})
+*/
 	});	
 
 	socket.on('disconnect', () => {
