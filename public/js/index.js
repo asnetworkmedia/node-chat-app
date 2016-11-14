9b44e5ccd4343b691ffcef016bb1043500dba1ae
Hamid Shahrokhi <Hamid Shahrokhi>
2016-11-13 21:59:08 -0500

    Emit newMessage on creatMessage

--- public/js/index.js
+++ public/js/index.js
@@ -7,10 +7,6 @@ socket.on('connect', function() {
 		console.log('Disconnected form server');
 	});
 
-	socket.emit('createMessage', {
-		from: 'hamid',
-		text: 'Not much dude'	
-	});
 });
 
 socket.on('newMessage', function(message) {
